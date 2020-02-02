b8209c111b66af48f52c199e89f99a9043bb6f37
Anderson Anizio <anderson.anizio@gmail.com>
2020-02-02 12:07:49 -0300

    'Refactor'

--- jpg_to_png_converter.py
+++ jpg_to_png_converter.py
@@ -5,23 +5,33 @@ from PIL import Image
 def is_jpg_file(filename):
     return filename.endswith(".jpg") or filename.endswith(".jpeg")
 
-def save_jpg_to_png(filename, source_folder, destination_folder):
-    source_filename = os.path.join(source_folder, filename)
+def jpg_to_png_path(filename, source_folder, destination_folder):
+    source_filepath = os.path.join(source_folder, filename)
     no_ext_filename = os.path.splitext(filename)[0]
     png_filename = no_ext_filename + ".png"
-    dest_filename = os.path.join(destination_folder, png_filename)
+    dest_filepath = os.path.join(destination_folder, png_filename)
+    return (source_filepath, dest_filepath)
 
-    im = Image.open(source_filename)
-    im.save(dest_filename)
+def save_image(source_path, destination_path):
+    im = Image.open(source_path)
+    im.save(destination_path)
 
-def convert_folder_jpg_to_png(source_folder, destination_folder):
+def create_dir_if_not_exists(path):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
+
+def save_jpg_file_to_png(filename, source_folder, destination_folder):
+    SOURCE_IDX, DEST_IDX = (0, 1)
+    filepaths = jpg_to_png_path(file, source_folder, destination_folder)
+    print(f"Converting {file}")
+    save_image(filepaths[SOURCE_IDX], filepaths[DEST_IDX])
+
+def convert_folder_jpg_to_png(source_folder, destination_folder):
+    create_dir_if_not_exists(destination_folder)
     for file in os.listdir(source_folder):
         if not is_jpg_file(file):
             continue
-        print(f"Converting {file}")
-        save_jpg_to_png(file, source_folder, destination_folder)
+    save_jpg_file_to_png(file, source_folder, destination_folder)
 
 def main():
     REQUIRED_LEN_ARGS = 3
